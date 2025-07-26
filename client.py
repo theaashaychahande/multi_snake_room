588e3490f5b268be4786c892be85af75224a82b8
Aashay Chahande <aashaychahande1717@gmail.com>
2025-07-26 05:38:59 +0530

    Created client.py

---
+++ client.py
@@ -0,0 +1,14 @@
+import socket
+import pickle
+import pygame
+import sys
+import math
+
+WIDTH, HEIGHT = 800, 600
+SNAKE_SPEED = 8
+TICK_RATE = 15
+
+
+SERVER_IP = '127.0.0.1' 
+SERVER_PORT = 5555
+
